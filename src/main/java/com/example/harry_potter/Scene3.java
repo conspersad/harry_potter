b3f4f5444ae466270d18f8a1b41c910b9e6c74a8
conspersad <117267836+conspersad@users.noreply.github.com>
2023-05-10 23:34:37 +0200

    Add class  wizard

--- src/main/java/com/example/harry_potter/Scene3.java
+++ src/main/java/com/example/harry_potter/Scene3.java
@@ -1,8 +1,13 @@
 package com.example.harry_potter;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
+import javafx.scene.control.Alert;
+import javafx.stage.Stage;
 
 
 public class Scene3 {
@@ -12,22 +17,26 @@ public class Scene3 {
     @FXML
     private Label The_philosopher_stone;
 
-
     @FXML
     private Label The_philosopher_stone_Intro;
+    @FXML
+    private Label Label_suite_intro;
     Wizard wizard=new Wizard(HelloController.getName(),100,0,Scene2.getHouse(),HelloController.getPet(), HelloController.getCore());
 
-    private void Intro_scene(){
-        String existingText = The_philosopher_stone_Intro.getText();
-        String newText = existingText +  wizard.getName() + " : Do you know what happend to Nevile ? He never miss potion class !\n" +
-                "Hermione: Well, i saw him coming back from the toilets of the donjon yesterdays he looked very scared, it's seems like he saw a ghost!!\n" +
-                wizard.getName()+" : I should probably find him ! \n" +
-                wizard.getName()+" : I'm gonna find where he is hiding !\n"+
-                wizard.getName()+" : Are you here..\n"+
-                wizard.getName()+": Ho my god, WHAT HAPPEND HERE !!!\n"+
-                "Nevile: I'm SORRY\nNevile: I'm SORRY\nNevile: I heard weird noise in the toilet yesterday night so i passe again today but\nNevile: I'm  didn't know it was a\nNevile: TROLL !!!\nNevile: You have to use Wingardium Leviosa !!!\n ";
-        The_philosopher_stone_Intro.setText(newText);
-        window.getChildren().addAll(The_philosopher_stone,The_philosopher_stone_Intro);
-    }
 
+    private void Intro_scene(){
+        window.getChildren().addAll(The_philosopher_stone, The_philosopher_stone_Intro, Label_suite_intro);
+    }
+    public void StopGame() {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setHeaderText(null);
+        alert.setContentText("Thank for playing !");
+        Stage stage = (Stage) window.getScene().getWindow();
+        stage.close();
+    }
+    @FXML
+    private void Fight() {
+        window.getChildren().clear();
+        Label choose_pet = new Label("Please your compagnon for the adventure !");
+    }
 }
