45078e0a07ebbe89a27a0b9fadd4c67ba9f346bd
conspersad <117267836+conspersad@users.noreply.github.com>
2023-05-10 09:20:10 +0200

    Add class  wizard

--- src/main/java/com/example/harry_potter/Scene3.java
+++ src/main/java/com/example/harry_potter/Scene3.java
@@ -18,7 +18,16 @@ public class Scene3 {
     Wizard wizard=new Wizard(HelloController.getName(),100,0,Scene2.getHouse(),HelloController.getPet(), HelloController.getCore());
 
     private void Intro_scene(){
-        window.getChildren().removeAll(The_philosopher_stone,The_philosopher_stone_Intro);
+        String existingText = The_philosopher_stone_Intro.getText();
+        String newText = existingText +  wizard.getName() + " : Do you know what happend to Nevile ? He never miss potion class !\n" +
+                "Hermione: Well, i saw him coming back from the toilets of the donjon yesterdays he looked very scared, it's seems like he saw a ghost!!\n" +
+                wizard.getName()+" : I should probably find him ! \n" +
+                wizard.getName()+" : I'm gonna find where he is hiding !\n"+
+                wizard.getName()+" : Are you here..\n"+
+                wizard.getName()+": Ho my god, WHAT HAPPEND HERE !!!\n"+
+                "Nevile: I'm SORRY\nNevile: I'm SORRY\nNevile: I heard weird noise in the toilet yesterday night so i passe again today but\nNevile: I'm  didn't know it was a\nNevile: TROLL !!!\nNevile: You have to use Wingardium Leviosa !!!\n ";
+        The_philosopher_stone_Intro.setText(newText);
+        window.getChildren().addAll(The_philosopher_stone,The_philosopher_stone_Intro);
     }
 
 }
--- src/main/java/com/example/harry_potter/Wizard.java
+++ src/main/java/com/example/harry_potter/Wizard.java
@@ -26,6 +26,7 @@ public class Wizard extends Character {
     public int defend() {
         return 0;
     }
+    public String getName(){return name;}
 
 /*
     @Override
